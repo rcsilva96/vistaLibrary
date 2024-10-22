29454512d6101920ea5e8fd05f150edaf6abc15d
Renato Carvalho Silva <rsilv1822@gmail.com>
2024-10-22 15:01:07 -0300

    book component now can show book information, also author's name is formatted to uppercase

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { Book } from '../../Book';
 
 @Component({
+
   selector: 'app-book',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book.component.html',
   styleUrl: './book.component.css'
+
 })
+
 export class BookComponent {
+
+  book : Book = {
+    id: 1,
+    title: "Uma busca implacável: Celso Russomano busca justiça!",
+    author: "Renato Carvalho Silva",
+    price: 88.80
+  }
 
 }
