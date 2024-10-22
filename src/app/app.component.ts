b1f1d5bc21bb8e4515b30f198570a049a7522642
Renato Carvalho Silva <rsilv1822@gmail.com>
2024-10-22 07:24:52 -0300

    added book component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { BookComponent } from './components/book/book.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HeaderComponent],
+  imports: [RouterOutlet, RouterModule, HeaderComponent, BookComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
